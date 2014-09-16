9a46cdacec734667fee45d63c6a6cfd505070a76
M3i <mihaiorzata@gmail.com>
2014-09-16 17:35:42 +0300

    Test Windows app for GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## First commit to test the Windows app for GitHub
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
